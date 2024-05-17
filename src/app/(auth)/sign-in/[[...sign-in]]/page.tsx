f9c8ec3b8f6e62875e7462e5b5491af02218fc97
DracoR22 <rdraco039@gmail.com>
2024-05-16 22:17:51 -0500

    added logo and favicon

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,30 @@
-import { SignIn } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2Icon } from "lucide-react"
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+           <h1 className="font-bold text-3xl -">
+            Welcome Back!
+           </h1>
+           <p className="text-base text-muted-foreground">
+            Log in or Create account to get back to your dashboard!
+           </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2Icon className="animate-spin text-muted-foreground"/>
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-primary hidden lg:flex items-center justify-center">
+
+      </div>
+    </div>
+  )
 }

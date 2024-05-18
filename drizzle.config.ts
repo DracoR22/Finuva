c65a8de09024803c824d2a4f1b8723981d906281
DracoR22 <rdraco039@gmail.com>
2024-05-18 11:47:58 -0500

    drizzle scripts added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -14,5 +14,7 @@ export default {
     schema: './src/db/schema.ts',
     dbCredentials: {
         url: process.env.DATABASE_URL!
-    }
+    },
+    verbose: true,
+    strict: true
 } satisfies Config

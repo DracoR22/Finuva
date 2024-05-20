895295b3e774ea7e53a72cff05d51422f8c42a0c
DracoR22 <rdraco039@gmail.com>
2024-05-20 10:12:52 -0500

    accounts completed

--- src/components/accounts/actions.tsx
+++ src/components/accounts/actions.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { EditIcon, MoreHorizontalIcon } from "lucide-react"
+import { EditIcon, MoreHorizontalIcon, TrashIcon } from "lucide-react"
 import { Button } from "../ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account"
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account"
+import { useConfirm } from "@/hooks/use-confirm"
 
 type Props = {
     id: string
@@ -11,10 +13,23 @@ type Props = {
 
 const Actions = ({ id }: Props) => {
 
+    const [ConfirmDialog, confirm] = useConfirm('Are you sure?', 'You are about to delete this transaction.')
+
     const { onOpen } = useOpenAccount()
+
+    const deleteMutation = useDeleteAccount(id)
+
+    const handleDelete = async () => {
+      const ok = await confirm()
+
+      if (ok) {
+        deleteMutation.mutate()
+      }
+    }
 
   return (
     <>
+      <ConfirmDialog/>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
            <Button variant={'ghost'} className="size-8 p-0">
@@ -22,9 +37,13 @@ const Actions = ({ id }: Props) => {
            </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-            <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={() => onOpen(id)}>
              <EditIcon className="size-4 mr-2"/>
                Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={handleDelete}>
+             <TrashIcon className="size-4 mr-2"/>
+               Delete
             </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>

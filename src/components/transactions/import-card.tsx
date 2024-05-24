b9990d875513ccb7029c101d964098368b67592c
DracoR22 <rdraco039@gmail.com>
2024-05-23 22:33:26 -0500

    import table done

--- src/components/transactions/import-card.tsx
+++ src/components/transactions/import-card.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import ImportTable from "./import-table"
+
 const dateFormat = 'yyyy-MM-dd HH:mm:ss'
 const outputFormat = 'yyyy-MM-dd'
 
@@ -18,10 +23,55 @@ type Props = {
 }
 
 const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
+
+    const [selectedColumns, setSelectedColumns] = useState<SelectedColumnState>({})
+
+    const headers = data[0]
+    const body = data.slice(1)
+
+    const onTableHeadSelectChange = (columnIndex: number, value: string | null) => {
+        setSelectedColumns((prev) => {
+            const newSelectedColumns = {...prev}
+
+            for (const key in newSelectedColumns) {
+                if (newSelectedColumns[key] === value) {
+                    newSelectedColumns[key] = null
+                }
+            }
+
+            if (value === 'skip') {
+                value = null
+            }
+
+            newSelectedColumns[`column_${columnIndex}`] = value
+
+            return newSelectedColumns
+        })
+    }
+
+    const progress = Object.values(selectedColumns).filter(Boolean).length
+
   return (
-    <div>
-      
-    </div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+    <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+             <CardTitle className="text-xl line-clamp-1">
+               Import Transaction
+            </CardTitle>
+           <div className="flex items-center gap-x-2">
+            <Button onClick={onCancel}>
+               Cancel
+            </Button>
+            <Button>
+               Continue ({progress} / {})
+            </Button>
+           </div>
+        </CardHeader>
+        <CardContent>
+            <ImportTable headers={headers} body={body} selectedColumns={selectedColumns} onTableHeadSelectChange={onTableHeadSelectChange}/>
+        </CardContent>
+        </Card>
+        </div>
   )
 }
 
---
+++ src/components/transactions/table-head-select.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+
+type Props = {
+    columnIndex: number
+    selectedColumns: Record<string, string | null>
+    onChange: (columnIndex: number, value: string | null) => void
+}
+
+const options = [
+    'amount',
+    'payee',
+    'notes',
+    'date'
+]
+
+const TableHeadSelect = ({ columnIndex, onChange, selectedColumns }: Props) => {
+
+    const currentSelection = selectedColumns[`column_${columnIndex}`]
+
+  return (
+    <Select value={currentSelection || ''} onValueChange={(value) => onChange(columnIndex, value)}>
+       <SelectTrigger className={cn('focus:ring-offset-0 focus:ring-transparent outline-none border-none bg-transparent capitalize',
+       currentSelection && 'text-purple-500')}>
+          <SelectValue placeholder="Skip"/>
+       </SelectTrigger>
+       <SelectContent>
+        <SelectItem value="skip">Skip</SelectItem>
+        {options.map((option, index) => {
+          const disabled = Object.values(selectedColumns).includes(option) && selectedColumns[`column_${columnIndex}`] !== option
+          return (
+          <SelectItem key={index} value={option} disabled={disabled} className="capitalize">
+            {option}
+          </SelectItem>
+          )})}
+       </SelectContent>
+    </Select>
+  )
+}
+
+export default TableHeadSelect

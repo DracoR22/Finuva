e988bea3b8cfa167e99b063a0bf3f6f7b4a83e82
DracoR22 <rdraco039@gmail.com>
2024-05-25 19:23:51 -0500

    pie charts done

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
+import DataCharts from "@/components/dashboard/data-charts"
 import DataGrid from "@/components/dashboard/data-grid"
 
 const HomeDashboardPage = () => {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
        <DataGrid/>
-       {/* <DataCharts/> */}
+       <DataCharts/>
     </div>
   )
 }
